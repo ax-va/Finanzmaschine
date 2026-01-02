d5fbb07c36418592ecaa7b413eefa0ded153db4a
ax-va <vasiliev.informatik@t-online.de>
2026-01-02 23:16:23 +0100

    update

---
+++ input_eth.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+class Mode(Enum):
+    VACUUM = "VACUUM"
+    GRAY = "GRAY"
+    HUNTER = "HUNTER"
+
+
+def detect_mode(price, vacuum_limit, gray_upper) -> Mode:
+    if price <= vacuum_limit:
+        return Mode.VACUUM
+    elif price <= gray_upper:
+        return Mode.GRAY
+    else:
+        return Mode.HUNTER
+
+
+if __name__ == "__main__":
+    local_high = 4000
+    take_profit = 0.2
+    loss_p = take_profit * 2
+    vacuum_limit = local_high * (1 - loss_p)
+    print("vacuum_limit:", vacuum_limit)
+
+    etp_price_bid = 2610  # sell
+    etp_price_ask = 2613  # buy
+    etp_price_spread = etp_price_ask - etp_price_bid
+    print("etp_price_spread:", etp_price_spread)
+
+    gray_upper = vacuum_limit / (1 - take_profit)
+    print("gray_upper:", gray_upper)
+
+    mode = detect_mode(etp_price_ask, vacuum_limit, gray_upper)
+    print("mode:", mode)
+
+    limit_order = etp_price_ask * (1 + take_profit)
+    print("limit_order:", limit_order)
+
+    stop_loss = max(vacuum_limit, etp_price_ask * (1 - loss_p))
+    print("stop_loss:", stop_loss)
+
+    cash_input = 0
+    etp_amount_bought = cash_input / etp_price_ask
+    print("etp_amount_bought:", etp_amount_bought)
