c5a96213d506a975f4f9adea56250743377f1113
ax-va <vasiliev.informatik@t-online.de>
2026-01-03 20:32:09 +0100

    update

--- core/share_lots.py
+++ core/share_lots.py
@@ -6,5 +6,5 @@ class GB00BLD4ZM24(ShareLot):
         super().__init__(
             share_isin="GB00BLD4ZM24",
             share_name="CoinShares Physical Staked Ethereum",
-            asset_name="ETH"
+            asset_name="ETH",
         )
