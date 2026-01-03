7ed53608e9d36fd5944edacbb6a2b0ba99b6a048
ax-va <vasiliev.informatik@t-online.de>
2026-01-03 16:19:46 +0100

    update

--- quote.py
+++ quote.py
@@ -6,8 +6,8 @@ class Quote:
         self.data = {
             "GB00BLD4ZM24": {
                 datetime.datetime(year=2026, month=1, day=2, hour=16, minute=15): {
-                    "bid_eur": 2610,
-                    "ask_eur": 2613,
+                    "bid_eth_eur": 2610,
+                    "ask_eth_eur": 2613,
                 }
             }
         }
