5790cf65be90bef9dc035475acfb31ac19b7c69b
ax-va <vasiliev.informatik@t-online.de>
2026-01-03 18:12:14 +0100

    update

--- core/quote.py
+++ core/quote.py
@@ -5,7 +5,7 @@ class Quote:
     def __init__(self) -> None:
         self.data = {
             "GB00BLD4ZM24": {
-                datetime.datetime(year=2026, month=1, day=2, hour=16, minute=15): {
+                "2026-01-02 16:15": {
                     "ETP": {
                         "bid_eur": None,
                         "ask_eur": None,
---
+++ etp/gb00bld4zm24.py
@@ -0,0 +1,10 @@
+from core.share_lot import ShareLot
+
+
+class GB00BLD4ZM24(ShareLot):
+    def __init__(self):
+        super().__init__(
+            isin="GB00BLD4ZM24",
+            share_name="ETP CoinShares Physical Staked Ethereum",
+            asset_name="ETH"
+        )
