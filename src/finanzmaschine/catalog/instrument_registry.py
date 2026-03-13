1adf79ef507ea9c0a38f55149e8b92e61bf3421a
ax-va <vasiliev.informatik@t-online.de>
2026-03-14 00:40:36 +0100

    update

--- src/finanzmaschine/catalog/instrument_registry.py
+++ src/finanzmaschine/catalog/instrument_registry.py
@@ -5,12 +5,14 @@ from finanzmaschine.catalog.asset_enum import Asset
 from finanzmaschine.catalog.exchange_enum import Exchange
 from finanzmaschine.core.market.instrument import Instrument
 
+ExchangeTicker = Tuple[Exchange, str]
+
 
 class InstrumentRegistry:
 
     def __init__(self):
         self._by_isin: Dict[str, Instrument] = {}
-        self._by_exchange_and_ticker: Dict[Tuple[Exchange, str], Instrument] = {}
+        self._by_exchange_ticker: Dict[ExchangeTicker, Instrument] = {}
         self._by_asset: DefaultDict[Asset, List[Instrument]] = defaultdict(list)
         self._by_wkn: Dict[str, Instrument] = {}
 
@@ -22,28 +24,28 @@ class InstrumentRegistry:
         self._by_isin[instrument.isin] = instrument
 
         for exchange, ticker in instrument.tickers.items():
-            self._by_exchange_and_ticker[(exchange, ticker)] = instrument
+            self._by_exchange_ticker[(exchange, ticker)] = instrument
 
         asset = getattr(instrument, "asset", None)
         if asset is not None:
             self._by_asset[asset].append(instrument)
 
-        wkn = self._get_wkn(instrument)
+        wkn = self._extract_wkn(instrument)
         if wkn is not None:
             self._by_wkn[wkn] = instrument
 
-    def by_isin(self, isin: str) -> Instrument:
+    def get_by_isin(self, isin: str) -> Instrument:
         return self._by_isin.get(isin)
 
-    def by_exchange_and_ticker(self, exchange: Exchange, ticker: str) -> Instrument:
-        return self._by_exchange_and_ticker.get((exchange, ticker))
+    def get_by_exchange_ticker(self, exchange: Exchange, ticker: str) -> Instrument:
+        return self._by_exchange_ticker.get((exchange, ticker))
 
-    def by_asset(self, asset: Asset) -> List[Instrument]:
+    def get_by_asset(self, asset: Asset) -> List[Instrument]:
         return self._by_asset.get(asset, [])
 
-    def by_wkn(self, wkn: str) -> Instrument:
+    def get_by_wkn(self, wkn: str) -> Instrument:
         return self._by_wkn.get(wkn)
 
     @staticmethod
-    def _get_wkn(instrument: Instrument):
+    def _extract_wkn(instrument: Instrument):
         return instrument.country_data.get("DE", {}).get("WKN")
--- src/finanzmaschine/sandbox.py
+++ src/finanzmaschine/sandbox.py
@@ -2,11 +2,11 @@ from finanzmaschine.catalog import registry
 from finanzmaschine.catalog.asset_enum import Asset
 from finanzmaschine.catalog.exchange_enum import Exchange
 
-instrument = registry.by_isin("GB00BLD4ZM24")
+instrument = registry.get_by_isin("GB00BLD4ZM24")
 print(instrument)
-instrument = registry.by_exchange_and_ticker(Exchange.EIX, "CETH")
+instrument = registry.get_by_exchange_ticker(Exchange.EIX, "CETH")
 print(instrument)
-instrument = registry.by_asset(Asset.ETH)
+instrument = registry.get_by_asset(Asset.ETH)
 print(instrument)
-instrument = registry.by_wkn("A3GQ2N")
+instrument = registry.get_by_wkn("A3GQ2N")
 print(instrument)
