2307fa5bd5abfb80e727b61000f10fb9eb6b16cf
ax-va <vasiliev.informatik@t-online.de>
2026-03-28 12:51:26 +0100

    update

--- src/finanzmaschine/sandbox.py
+++ src/finanzmaschine/sandbox.py
@@ -8,16 +8,3 @@ print(etp)
 
 assets = asset_registry.get_all()
 pprint(assets)
-# [Asset(id='BTC', name='Bitcoin'),
-#  Asset(id='ETH', name='Ethereum'),
-#  Asset(id='NEAR', name='NEAR Protocol'),
-#  Asset(id='SOL', name='Solana'),
-#  Asset(id='TON', name='Toncoin'),
-#  Asset(id='EUR', name='Euro'),
-#  Asset(id='USD', name='United States Dollar'),
-#  Etp(id='GB00BLD4ZM24',
-#      name='CoinShares Physical Staked Ethereum',
-#      underlying_asset=Asset(id='ETH', name='Ethereum')),
-#  Etp(id='CH1297762812',
-#      name='21Shares Toncoin Staking ETP',
-#      underlying_asset=Asset(id='TON', name='Toncoin'))]
