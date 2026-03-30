60b48a6b278c8aa970f0d372d415bed7d0f8b2bd
ax-va <vasiliev.informatik@t-online.de>
2026-03-30 20:52:33 +0200

    update

--- src/finanzmaschine/portfolio/lots/priced_lot.py
+++ src/finanzmaschine/portfolio/lots/priced_lot.py
@@ -4,6 +4,7 @@ from finanzmaschine.portfolio.assets import BaseAsset
 from finanzmaschine.portfolio.lots.base_lot import BaseLot
 from finanzmaschine.portfolio.records.non_sale_reduction_record import NonSaleReductionRecord
 from finanzmaschine.portfolio.records.priced_record import PricedRecord
+from finanzmaschine.utils.float_helper import safe_sum
 
 A = TypeVar('A' , bound=BaseAsset)
 N = TypeVar("N", bound=NonSaleReductionRecord)
@@ -11,6 +12,13 @@ P = TypeVar("P", bound=PricedRecord)
 
 
 class PricedLot(BaseLot[A, N | P, P]):
+
+    @property
+    def quantity_realized(self) -> float:
+        return safe_sum(
+            (r_out.quantity for r_out in self.records_out if isinstance(r_out, PricedRecord)),
+            float_eps=0.0,
+        )
 
     @property
     def initial_cost_basis(self) -> float:
@@ -25,5 +33,5 @@ class PricedLot(BaseLot[A, N | P, P]):
         return self.initial_cost_basis / record_in.quantity
 
     @property
-    def open_cost_basis(self) -> float:
-        return self.quantity_open * self.unit_cost_basis
+    def realized_cost_basis(self) -> float:
+        return self.quantity_realized * self.unit_cost_basis
