bfb1db2393c3d89869e2bbae4349205690a10729
ax-va <vasiliev.informatik@t-online.de>
2026-04-04 20:10:47 +0200

    update

--- src/finanzmaschine/portfolio/positions/security_position.py
+++ src/finanzmaschine/portfolio/positions/security_position.py
@@ -1,7 +1,8 @@
-from typing import TypeVar
+from typing import TypeVar, Generic
 
 from finanzmaschine.portfolio.assets.security import Security
 from finanzmaschine.portfolio.lots.security_lot import SecurityLot
+from finanzmaschine.portfolio.operation_types.non_trade_increase_type import NonTradeIncreaseType
 from finanzmaschine.portfolio.positions.acquisition_position import AcquisitionPosition
 from finanzmaschine.portfolio.records.non_trade_decrease_record import NonTradeDecreaseRecord
 from finanzmaschine.portfolio.records.non_trade_increase_record import NonTradeIncreaseRecord
@@ -14,5 +15,8 @@ T = TypeVar("T", bound=SecurityTradeRecord)
 L = TypeVar("L", bound=SecurityLot)
 
 
-class SecurityPosition(AcquisitionPosition[S, D, I, T, L]):
+class SecurityPosition(
+    AcquisitionPosition[S, NonTradeDecreaseRecord, NonTradeIncreaseRecord[S, NonTradeIncreaseType], T, L],
+    Generic[S, T, L],
+):
     pass
