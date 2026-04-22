3e377ef387465a0ee18a918aba96c50536e9b84f
ax-va <vasiliev.informatik@t-online.de>
2026-04-22 21:45:08 +0200

    smoke_test added

---
+++ tests/integration_tests/conftest.py
@@ -0,0 +1,82 @@
+from decimal import Decimal
+
+import polars as pl
+import pytest
+
+from finanzmaschine.catalog import asset_registry
+from finanzmaschine.portfolio.assets import CryptoEtp
+from finanzmaschine.portfolio.operation_types import TradeType
+from finanzmaschine.portfolio.positions import CryptoEtpPosition
+from finanzmaschine.portfolio.records import Direction, CryptoEtpTradeRecord
+
+
+@pytest.fixture(scope="session")
+def df_ton_etp_fifo(data_dir) -> pl.DataFrame:
+    df = pl.read_csv(data_dir / "etps/ton_etp_fifo.csv",
+        try_parse_dates=True,
+        schema_overrides={
+          "quantity_open_before": pl.String,
+          "quantity_to_close": pl.String,
+          "quantity_open_after": pl.String,
+          "quantity_closed": pl.String,
+          "quantity_remaining": pl.String,
+          "fee_to_close": pl.String,
+          "fee_closed": pl.String,
+          "fee_remaining": pl.String,
+          "proceeds": pl.String,
+          "cost_basis_sold": pl.String,
+          "pnl": pl.String,
+        },
+    )
+    return df
+
+@pytest.fixture(scope="session")
+def df_ton_etp_trade(data_dir) -> pl.DataFrame:
+    df = pl.read_csv(data_dir / "etps/ton_etp_trade.csv",
+        try_parse_dates=True,
+        schema_overrides={
+            "price": pl.String,
+            "fee": pl.String,
+            "base_asset_flow": pl.String,
+            "quote_asset_flow": pl.String,
+        },
+    ).sort("datetime")
+    return df
+
+@pytest.fixture(scope="session")
+def ton_etp_position_fifo(df_ton_etp_trade) -> CryptoEtpPosition:
+    base_asset: CryptoEtp = asset_registry.get("CH1297762812")
+    position = CryptoEtpPosition(base_asset=base_asset)
+
+    for row in df_ton_etp_trade.iter_rows(named=True):
+        base_asset_flow = row["base_asset_flow"]
+        quantity = abs(Decimal(base_asset_flow))
+        datetime = row["datetime"]
+        direction = Direction.OUT if base_asset_flow.startswith("-") else Direction.IN
+        operation_type = TradeType(row["operation_type"])
+
+        quote_asset = asset_registry.get(row["quote_asset_id"])
+        price = Decimal(row["price"])
+        fee = Decimal(row["fee"])
+
+        broker = row["broker"]
+        order_id = row["order_id"]
+        exchange = row["exchange"]
+        trade_id = row["trade_id"]
+
+        record = CryptoEtpTradeRecord(
+            quantity=quantity,
+            datetime=datetime,
+            direction=direction,
+            operation_type=operation_type,
+            quote_asset=quote_asset,
+            price=price,
+            fee=fee,
+            broker=broker,
+            order_id=order_id,
+            exchange=exchange,
+            trade_id=trade_id,
+        )
+        position.apply(record, "FIFO")
+
+    return position
---
+++ tests/integration_tests/test_positions.py
@@ -0,0 +1,90 @@
+from decimal import Decimal
+from typing import TypeVar
+
+import polars as pl
+import pytest
+
+from finanzmaschine.portfolio.positions.acquisition_position import AcquisitionPosition
+from finanzmaschine.utils.decimal_helper import round_to_quantum
+
+P = TypeVar("P", bound="AcquisitionPosition")
+
+
+@pytest.fixture(scope="function")
+def position(request) -> P:
+    return request.getfixturevalue(request.param)
+
+
+@pytest.fixture(scope="function")
+def golden_values(request) -> pl.DataFrame:
+    return request.getfixturevalue(request.param)
+
+
+@pytest.mark.parametrize(
+    "position,golden_values,proceeds,cost_basis_sold,pnl",
+    [
+        ("ton_etp_position_fifo", "df_ton_etp_fifo", Decimal("5065.52"), Decimal("5621.84"), Decimal("-556.32")),
+    ],
+    indirect=["position", "golden_values"],
+)
+def test_close_position(
+        position: P,
+        golden_values: pl.DataFrame,
+        proceeds: Decimal,
+        cost_basis_sold: Decimal,
+        pnl: Decimal,
+):
+    # workaround for typechecker
+    position: AcquisitionPosition
+
+    position_proceeds = Decimal("0")
+    position_cost_basis_sold = Decimal("0")
+    position_pnl = Decimal("0")
+
+    i = 0
+    for lot in position.lots_with_records_sold:
+
+        lot_proceeds = Decimal("0")
+        lot_cost_basis_sold = Decimal("0")
+        lot_pnl = Decimal("0")
+
+        for record in lot.records_sold:
+            assert record.quantity == Decimal(golden_values.row(i, named=True)["quantity_closed"])
+            assert record.fee == Decimal(golden_values.row(i, named=True)["fee_closed"])
+
+            record_proceeds = round_to_quantum(
+                record.quantity * record.price - record.fee,
+                lot.record_in.quote_asset.quantum,
+            )
+            assert record_proceeds == Decimal(golden_values.row(i, named=True)["proceeds"])
+
+            record_cost_basis_sold = round_to_quantum(
+                record.quantity / lot.record_in.quantity * lot.cost_basis,
+                lot.record_in.quote_asset.quantum,
+            )
+            assert record_cost_basis_sold == Decimal(golden_values.row(i, named=True)["cost_basis_sold"])
+
+            record_pnl = record_proceeds - record_cost_basis_sold
+            assert record_pnl == Decimal(golden_values.row(i, named=True)["pnl"])
+
+            lot_proceeds += Decimal(golden_values.row(i, named=True)["proceeds"])
+            lot_cost_basis_sold += Decimal(golden_values.row(i, named=True)["cost_basis_sold"])
+            lot_pnl += Decimal(golden_values.row(i, named=True)["pnl"])
+
+            i += 1
+
+        assert lot.proceeds == lot_proceeds
+        assert lot.cost_basis_sold == lot_cost_basis_sold
+        assert lot.pnl == lot_pnl
+
+        position_proceeds += lot_proceeds
+        position_cost_basis_sold += lot_cost_basis_sold
+        position_pnl += lot_pnl
+
+    assert position.proceeds == proceeds
+    assert position.cost_basis_sold == cost_basis_sold
+    assert position.pnl == pnl
+
+    assert position.proceeds == position_proceeds
+    assert position.cost_basis_sold == position_cost_basis_sold
+    assert position.pnl == position_pnl
