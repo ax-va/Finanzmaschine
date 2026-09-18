f4b174d91fdd5f3ee67ee48a0bdc4e6d393317d8
ax-va <vasiliev.informatik@t-online.de>
2026-09-18 23:52:54 +0200

    updated

--- py/finanzmaschine-staking/src/finanzmaschine_staking/orm/near/staking_snapshot.py
+++ py/finanzmaschine-staking/src/finanzmaschine_staking/orm/near/staking_snapshot.py
@@ -5,8 +5,8 @@ from sqlmodel import Field
 from finanzmaschine_staking.orm.near.balance_snapshot import BalanceSnapshot
 
 
-class ScopedBalanceSnapshot(BalanceSnapshot, table=True):
-    __tablename__ = 'near_staking_balance_snapshots'
+class StakingSnapshot(BalanceSnapshot, table=True):
+    __tablename__ = 'near_staking_snapshots'
 
     account_key: UUID = Field(primary_key=True)
     pool_id: str = Field(primary_key=True)
